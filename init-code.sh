e945921e868613244348ac521d7b255abfe3d53f
Haris Rotsos <cr409@cam.ac.uk>
2011-10-28 12:20:30 +0100

    adding a simple script to download unbound code and compile it

---
+++ init-code.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+CWD=`pwd`
+# download ldns code, required by the unbound server
+if [ ! -e ldns.svn ]; then
+  svn co http://nlnetlabs.nl/svn/ldns/trunk/ ldns.svn
+fi
+
+cd ldns.svn
+#update svn repo
+svn up
+
+if [ ! -e configure ]; then 
+  libtoolize -c --install
+  autoreconf --install
+fi 
+
+#compile data
+if [ ! -e Makefile ]; then
+  ./configure --disable-gost
+fi
+make all doc
+
+
+cd $CWD
+#download unbound code 
+if [ ! -e unbound.svn ]; then
+  svn co http://unbound.nlnetlabs.nl/svn/trunk/ unbound.svn
+fi
+
+cd unbound.svn
+#update svn repo
+svn up
+#compile data
+if [ ! -e Makefile ]; then
+  ./configure --disable-gost --with-ldns=../ldns.svn/
+fi
+make
+
+cd $CWD
