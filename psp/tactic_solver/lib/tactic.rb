b79c9e25b5726ffced86c327ddf57a60ed13fc71
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2011-11-18 18:30:50 +0000

    Dummy tactic solver
    Got crazy not doing anything real, so spent a few hours coding up a
    data flow tactic solver.
    If this is something we end up using or not, is not too important, but
    it was an interesting experience regardless.
    
    Tactics can be added to the tactics folder, and will automatically be
    used.

---
+++ psp/tactic_solver/lib/tactic.rb
@@ -0,0 +1,98 @@
+require 'yaml'
+
+class Tactic
+  include Bud
+
+  state do
+    scratch :possible_connections, [:interface, :to]
+  end
+  
+  def initialize name, tactic_solver, options = {}
+    puts "> initializing tactic: #{name}"
+    @name = name
+    @tactic_solver = tactic_solver
+    setup_tactic
+    super options
+  end
+
+  bloom do
+    ###
+  end
+
+  def post_setup
+    self.register_callback(:possible_connections) do |entries|
+      entries.each do |entry|
+        evaluate_input entry
+      end
+    end
+    self.run_bg
+  end
+
+  def input_to_evaluate interface, to
+    self.sync_do {
+      possible_connections <+ [[interface, to]]
+    }
+  end
+
+  private
+  def setup_tactic
+    config = YAML::load(File.open("tactics/#{@name}/config.yml"))
+    @prober = config['prober']
+    @actuator = config['actuator']
+    @background_process = config['background_process']
+    @description = config['description']
+    @supported_interfaces = config['supported_interfaces']
+
+    check_file_exists @prober, @actuator
+
+    # Setup background process
+    if @background_process then
+      status = `tactics/#{@name}/#{@background_process}`
+      unless status == "" then
+        print_error "background process failed to start: #{status}"
+      end
+    end
+
+  rescue Errno::ENOENT
+    print_error "Missing configuration file: Please ensure tactics/#{@name}/config.yml exists"
+
+  end
+
+  def check_file_exists *files
+    files.each do |file|
+      print_error "#{file} is missing" unless File.exists? "tactics/#{@name}/#{file}"
+    end
+  end
+
+  def evaluate_input entry
+    if @supported_interfaces.include?(entry.interface) then
+      puts "[#{@name}]: Evaluating possibility of connecting to #{entry.to} through #{entry.interface}"
+      result = `tactics/#{@name}/#{@prober} #{entry.interface} #{entry.to}`
+      if (result =~ /SUCCESS ([\d]*) ([\d]*) ([\d]*)/) != nil
+        latency = $1
+        bandwidth = $2
+        overhead = $3
+        data = {
+          :client => entry.to,
+          :strategy => @name,
+          :interface => entry.interface,
+          :latency => latency.to_i,
+          :bandwidth => bandwidth.to_i,
+          :overhead => overhead.to_i
+        }
+        puts "[#{@name}]: Can connect to #{entry.to} through #{entry.interface} with latency #{latency}, bandwidth #{bandwidth}."
+        @tactic_solver.update data
+
+      else
+        puts "[#{@name}]: Cannot connect to #{entry.to} through #{entry.interface}."
+
+      end
+    else
+      puts "[#{@name}]: Tactic does not support interface #{entry.interface}"
+    end
+  end
+
+  def print_error description
+      puts "ERROR [#{@name}]: #{description}"
+  end
+end
---
+++ psp/tactic_solver/tactic_solver.rb
@@ -0,0 +1,106 @@
+require 'rubygems'
+require 'bud'
+require "lib/tactic"
+require 'pp'
+
+class TacticSolver
+  include Bud
+
+  state do
+    table :path, [:from, :to, :cost] => [:next, :latency, :bandwidth, :overhead]
+    table :link, [:from, :to, :strategy, :interface] => [:latency, :bandwidth, :overhead]
+
+    table :shortest, [:from, :to] => [:cost, :next, :latency, :bandwidth, :overhead]
+  end
+
+  # recursive rules to define all paths from links
+  bloom :make_paths do
+    # base case: every link is a path
+    path <= link do |c| 
+      [c.from, c.to, link_cost(c), c.to, c.latency, c.bandwidth, c.overhead]
+    end
+    
+    # inductive case: make path of length n+1 by connecting a link to a path of
+    # length n
+    path <= (link * path).pairs(:to => :from) do |l,p|
+      [l.from, p.to, 
+       link_cost(l) + p.cost,
+       p.from, 
+       l.latency + p.latency, 
+       min(l.bandwidth, p.bandwidth), 
+       l.overhead + p.overhead]
+    end
+  end
+  
+  bloom :find_shortest do
+    # shortest <= path.argmin([path.from, path.to], path.cost)
+  end
+
+  def initialize options = {}
+    start_tactics
+    super options
+  end
+
+  def add_input interface, to
+    @tactics.each do |tactic|
+      tactic.input_to_evaluate interface, to
+    end
+  end
+
+  def update data
+    new_data = [[ip_port, data[:client], data[:strategy], data[:interface], data[:latency], data[:bandwidth], data[:overhead]]]
+    self.sync_do {
+      link <+ new_data
+    }
+  end
+
+  private
+  def link_cost l
+      10 * l.latency + 1000 / l.bandwidth + 10 * l.overhead # Find a way to compute cost...
+  end
+
+  def start_tactics
+    @tactics = []
+    # Find and initialize all tactics
+    Dir.foreach("tactics") do |dir_name|
+      @tactics << Tactic.new(dir_name, self) if File.directory?("tactics/#{dir_name}") and !(dir_name =~ /\.{1,2}/)
+    end
+    @tactics.each do |tactic| 
+      tactic.post_setup
+    end
+  end
+
+  def min a, b
+    a < b ? a : b
+  end
+
+  def max a, b
+    a < b ? b : a
+  end
+end
+
+tactic_solver = TacticSolver.new
+tactic_solver.run_bg
+tactic_solver
+
+tactic_solver.add_input "eth", "localhost"
+tactic_solver.add_input "bluetooth", "localhost"
+
+# Add some dummy data:
+tactic_solver.sync_do {
+  tactic_solver.link <+- [["macbook", "smartphone", "direct_connection", "eth", 200, 2000, 0],
+                         ["macbook", tactic_solver.ip_port, "direct_connection", "eth", 100, 30000, 0],
+                         ["work", "macbook", "direct_connection", "eth", 200, 300, 0],
+                         ["macbook", "homecomputer", "direct_connection", "eth", 100, 30000, 0],
+                         ["macbook", "ipad", "ssh", "eth", 130, 250, 14]]
+}
+
+# What are our shortest paths now?
+puts "\n\n----------------------------"
+puts "\nCurrent, shortest paths (Can't be calculated due to bug :|):"
+puts ":("
+tactic_solver.shortest.sort.each {|t| puts t.inspect}
+puts "\nLinks:"
+tactic_solver.link.sort.each {|t| puts t.inspect}
+puts "\nPaths:"
+tactic_solver.path.sort.each {|t| puts t.inspect}
