322f3aef7f501071024190b1607369aa321e3d8d
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2011-11-29 15:11:11 +0000

    Migrate the tactic_solver to routing_info
    The tactic_solver is now called routing_info. The new tactic_solver is
    instead intended to be a dynamic data flow construct that sets up
    connections on the fly and memorised stuff it needs to do so.

--- psp/tactic_solver/lib/tactic_protocol.rb
+++
@@ -1,8 +0,0 @@
-module TacticProtocol
-  state do
-    channel :eval_tactic_request
-    channel :tactic_evaluation_result
-    channel :tactic_signup
-    channel :failed_evaluation_result
-  end
-end
--- psp/tactic_solver/tactics/direct_connection/daemon.rb
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/ruby
-
-require 'rubygems' 
-require 'daemons'
-
-Daemons.run(File.dirname(__FILE__) + '/daemon_process.rb')
