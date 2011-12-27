ce4cf4882ae2f39ada242832cb922aebecec5370
Narseo Vallina Rodríguez <Narseo@narseo.(none)>
2011-12-27 01:37:45 +0100

    Changes in sqliteController Library, designed for storing and diseminating the public keys. To be tested properly. Several things still to be done such as fragmentation of public keys to be txed over twitter private messages

--- socialnet/personalSignpost.py
+++ socialnet/personalSignpost.py
@@ -2,106 +2,184 @@
 # coding: utf-8
 
 #Thread that is responsible for: 
-#a) Getting social net and cache it. Load it on boot
-#b) Check if a user is part of someone's social net
-#c) Generate keys for users
-#d) Store friends' keys
-#e) Extend address book (name, public key, social net accounts, affiliations)
+#a) Authentication
+#b) Getting social net and cache it (sqlite). Load it from db
+#c) Store friends' keys in db
+#d) Check if a user is part of someone's social net
+#e) Generate keys for users
+#f) Extend address book (name, public key, social net accounts, affiliations)
 
-#from twython import Twython
+
 
 from threading import Thread
+#TwitterChecker -> Public twitter API. no auth required but # requests limited
 from twitterChecker import TwitterChecker
+from sqliteController import SqliteController
 import json
 import time
 import os.path
 import tweepy
-import simplejson, oauth
 import webbrowser
-#from OauthAccess import OauthAccess
-#from OauthRequest import OauthRequest
-
-
-#Access token	410104745-5bHJ0GjL2touskpxDhanzXfTGnoGFiW8f39Y6Vd4
-#Access token secret	Gq9Qx2zg0W7ItfRxnTXfA4JtF9MJGHithtQoIdNtaI
-#Access level	Read-only
 
 class PersonalSignpost:
-
-	def __init__ (self, user_screen_name, password):
+	def __init__ (self, user_screen_name):
 		self.CONSUMER_KEY = "6HggnCjXRY2Z81bV8abcA"
 		self.CONSUMER_SECRET = "b0MSaJjSJwX51QPyjr1MMtrf00rBupD82rhdhVB8rM"
+		self.DBPATH = "./personalSp.db"
+		self.KEY_PATH = "./twitterKey.sp"
+		self.TWITTER_KEY_MSG_HEADER = "sp_pkey"
 		#Duty cycle used to update/find changes in social net
 		self.DUTY_CYCLE = 60;
 		print "Personal signpost"
 		self.user_screen_name = user_screen_name
 		#Local social_net cache:
-		self.password = password
 		print "PS for %s created" %(user_screen_name)
-		self.authenticate()
-		#Should load socialNet list from file (in memory)		
-		self.user_address_book_filename = user_screen_name + ".ab"
-		#Check if file exists
-		try
-			open(self.user_address_book_filename)
-			#Read file
-			
-		except IOError as e:
-			print "File %s doesn't exist" (% self.user_address_book_filename)
-			#Read input-output
-			
-		t = Thread (target = self.runFunc, args = ())
-		t.start()
+		self.authenticateTwitter()
+
+		#print "Creating DB"
+		self.sqliteController = SqliteController(self.DBPATH)
+		self.dbConnector = self.sqliteController.createDb()
 		
+		#If we want to get by default all the twitter accounts for a user, execute that
+		#try:
+		#	self.updateSocialNetTweepy()
+			
+		#except:
+		#	print "Error retrieving social net"
+		#	pass
+		
+		#TODO: Implement a GUI for control of the static policies and the groups?	
+			
+		#Run process					
+		#t = Thread (target = self.runFunc, args = ())
+		#t.start()
 		
 	#method to authenticate a user in twitter with Oauth
-	def authenticate (self):	
-		auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
-		try:
-			redirect_url = auth.get_authorization_url()
-		except tweepy.TweepError:
-			print 'Error! Failed to get request token.'
-		#print "Redirect URL -> %s" %(redirect_url)
-		#Redirect to the browser in order to get the Oauth pin (i.e. verifier)
-		new = 2 # open in a new tab, if possible
-		# open a public URL in the webbrowser
-		webbrowser.open(redirect_url,new=new)
-		verifier = raw_input('PIN Verifier:')
-		#Now send to twitter the verified token:
-		try:
-			auth.get_access_token(verifier)
-		except tweepy.TweepError:
-			print 'Error! Failed to get access token.'
+	def authenticateTwitter (self):
+			
+		if not os.path.exists(self.KEY_PATH):
+#			try:
+				auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
+				print "Getting authorization from the user"
+				redirect_url = auth.get_authorization_url()
+				#print "Redirect URL -> %s" %(redirect_url)
+				#Redirect to the browser in order to get the Oauth pin (i.e. verifier)
+				new = 2 # open in a new tab, if possible
+				# open a public URL in the webbrowser
+				webbrowser.open(redirect_url,new=new)
+				verifier = raw_input('PIN Verifier:')
+				try:
+					auth.get_access_token(verifier)
+				except tweepy.TweepError:
+					print 'Error! Failed to get access token.'
+				
+				f = open(self.KEY_PATH, 'w')
+				#f.write(auth.access_token.key)
+				print auth.access_token.key
+				print auth.access_token.secret
+				f.write(auth.access_token.key+','+auth.access_token.secret)
+				#f.write(auth.access_token.secret)
+				f.close()
+#			except tweepy.TweepError:
+#				print 'Error! Failed to get request token.'
+		else:
+			auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
+			print 'KEY ALREADY exists'
+			#READ TOKEN FROM FILE
+			f = open(self.KEY_PATH, 'r')
+			line = f.readline()
+			vals = str.split(line, ',')			
+			f.close()			
+			auth.set_access_token(vals[0], vals [1])		
+		
 		self.api = tweepy.API(auth)
 		#To test if it works, uncomment the following line (posting something in your twitter wall)
-		#newTweet = raw_input('Enter a new tweet: ')
-		#api.update_status(newTweet)
-	
+		
+	#Used to test authentication. Posts a new tweet entered by the user via its command line
+	def post_test (self):
+		newTweet = raw_input('Enter a new tweet: ')
+		self.api.update_status(newTweet)
+		
 	#thread. Checks periodically for changes in the social net. It should handle the requests to
 	#verify whatever happens in the social net.
 	def runFunc (self):
-		
+		#Check if there's a change in the social net but 1st need to have sqllite running
+		#TODO
 		self.twitterSignpost = TwitterChecker()
-		
 		while True:
-			print "user: %s - Value: %d" %(self.user_screen_name, i)
+			print "user: %s" %(self.user_screen_name)
 			time.sleep(self.DUTY_CYCLE)
-			
-		
-	def updateSocialNet (self):
-		#update user's social network
-		print "ready to update social net for %s" %(self.user_screen_name)
-		#check if user address book exists
-		if os.path.isfile (self.user_address_book_filename):
-			print "Address book file exists: %s" %(self.user_address_book_filename)
-			#Read address book and load it in memory
-		else:
-			print "Address book file doesn't exist %s" %(self.user_address_book_filename)
-			#if don't -> Create one and save it in a file
-			f = open ('./'+self.user_address_book_filename, 'w')
-			print "Making address book for: %s in %s" %(self.user_screen_name, f)
-			symmetricFriendship = self.twitterSignpost.getUserEgoNetwork(self.user_screen_name)
-			#Save it in a file. JSON format {user_id, user_real_name, user_screen_name, user_location, public_key, affiliation(s)}
-			print "List of friends: "
-			for friends_id in symmetricFriendship:
-				print friends_id
+			#Check changes in social net but needs to be integrated with Sebastian's
+			#Save data in sqlite
+	
+	#Updates the social net using Twitter's contacts			
+	def updateSocialNetTweepy (self):
+		print "ready to update social net for %s using tweepy" %(self.user_screen_name)		
+		print "Current list of users:"
+		self.sqliteController.printContacts()
+		#check if db has been created
+		if not os.path.exists(self.DBPATH):
+			print "Database has been removed. Creating a new one"
+			self.dbConnector = self.sqliteController.createDb()
+		#get a list of integers with friends and followers ids
+		friendsIds = self.api.friends_ids()
+		followersIds = self.api.followers_ids()
+		print "Number of friends %s" %(len(friendsIds))
+		print "Number of followers %s" %(len(followersIds))	
+		symmetricFriendship =set(followersIds).intersection(set(friendsIds))
+		print "SymmetricFriends: %s" %(len(symmetricFriendship))
+		#Save friends on database
+		for index in symmetricFriendship:
+			if self.sqliteController.checkIfUserExists(index) == 0:
+				print "User is not in DB. Retrieving data from Twitter and storing it in DB"
+				try:
+					userTmp = self.api.get_user(user_id=index)		
+					print "----"
+					print "Username: %s" %(userTmp.screen_name)
+					#Save user on database
+					print "Name: %s" %(userTmp.name)
+					self.sqliteController.createUser(userTmp.name, userTmp.screen_name, index)
+				except:
+					print "Error trying to retrieve user: %s" %(index)
+	
+	def sendKeyToUser(self, user, key):		
+		print "------"
+		print "Ready to send public key to %s" %(user)
+		toSend = self.TWITTER_KEY_MSG_HEADER+key		
+		print "Key: %s" %(toSend)
+		self.api.send_direct_message(screen_name=user, text=toSend)	
+		print "------"
+	
+	#Gets if there is any change in the most recent public keys
+	#TODO: It might be necessary to be changed as it only considers twitter-like names
+	#We can send other names by making a header (plus it also needs to do some sorf of
+	#packet fragmentation as the max size in twitter is 140 messages)
+	def getLatestKeys (self):
+		print "Ready to get latest public keys"
+		messages = self.api.direct_messages()
+		try:
+			messages = self.api.direct_messages()
+			counter = 1
+			for message in messages:
+				messageId = message.id
+				print "Message id%s: %s - %s" %(messageId, counter, message.text)
+				counter = counter + 1
+				if message.text.startswith(self.TWITTER_KEY_MSG_HEADER):
+					publicKey = message.text[7:]
+					print "Retrieved Public Key From %s: %s" %(message.sender_screen_name, publicKey)
+					#TODO: Save in database for this user
+					self.sqliteController.addKeyToUser(message.sender_screen_name, publicKey)		
+					self.api.destroy_direct_message(messageId)
+					print "Message should be removed"
+				else:
+					print "No signpost key"
+		except:
+			print "Could not retrieve messages"
+			pass
+
+	def searchPeople (self, query):
+		#TODO: Might be useful in order to expand someone's social net later but not at the moment.
+		#Similar to find people button in twitter. We could search for someone and then add him/her
+		#on the local address book
+		#tweepy API -> self.api.search_users()
+		print "Searching: %s" %(query)
--- socialnet/sqliteController.py
+++ socialnet/sqliteController.py
@@ -7,6 +7,7 @@ import datetime
 
 class SqliteController:
 	
+	#Wrapper for the Sqlite DB
 	def __init__ (self, dbPath):
 		self.dbPath = dbPath
 		
@@ -18,22 +19,27 @@ class SqliteController:
 			c = conn.cursor();
 			c.execute('''create table if not exists userIds (userId integer, signpostname text primary key, publickey text, timestamp date)''')
 			c.execute('''create table if not exists users (userId integer primary key, name varchar unique, twitterApiId varchar unique)''')
-			c.execute('''create table if not exists policies (policeId integer primary key, policyData text)''')
+			c.execute('''create table if not exists policies (policyId integer primary key, policyData text unique)''')
 			c.execute('''create table if not exists userpolicies  (userId integer, policyId integer)''')
 			#include default policy
-			c.execute("""insert into policies (policeId,policyData) values (0, 'ALL')""");			conn.commit()
+			c.execute("""insert into policies (policyId,policyData) values (0, 'ALL')""");
+			#TODO: Include all the triggers, specially when data is removed (e.g. users, etc)
+			#Trigger that removes the entries for a user on userIds and policies table
+			c.execute("""create trigger remove_user DELETE ON users BEGIN DELETE userIds where userId = old.userId; DELETE userpolicies where userId = old.userId; END""")
+			#Trigger that removes any reference on the userPolicies
+			c.execute("""create trigger remove_policy DELETE ON policies BEGIN DELETE userpolicies where policyId = old.policyId; END""")
+			conn.commit()
 			c.close()
 			print 'Tables created for the 1st time'
 		else:
+			#TODO: Is it necessary to create the triggers again if the tables are already defined?
 			print 'Tables already created'
-	
-	#Returns a policy for a given user
-	def findUserPolicy (username):
-		print 'Finding user policy for %s' %username
-		#TODO
-		
+
+
 	#Checks whether a given user (based on twitter Id) has been inserted in users' table
+	#Returns 1 (True) or 0 (False)
 	def checkIfUserExists (self, userId):
+		print "-----"
 		print "Check if user %s exists" %(userId)
 		conn = sqlite3.connect(self.dbPath)
 		c = conn.cursor();
@@ -48,6 +54,7 @@ class SqliteController:
 			
 	#Adds a user both the user table and the userId table
 	def createUser (self, username, signpostName, userId):
+		print "-----"
 		print 'Creating new user %s, %s %s' %(username, signpostName, userId)		
 		conn = sqlite3.connect(self.dbPath)
 		c = conn.cursor();		
@@ -57,14 +64,201 @@ class SqliteController:
 		#Try to get userId		
 		c.execute("select * from users where name=?", (username,))
 		entries = c.fetchone()
-		userId = entries[0]
+		userId = None
+		if not entries is None:
+			#there's an entry for this username
+			userId = entries[0]
 		print "Assigned UserId: %s" %(userId)		
 		c.execute("insert into userIds values (?, ?, ?, ?)", (userId, signpostName, None, now.strftime("%Y-%m-%d %H:%M:%s")))
 		conn.commit()
 		c.close()
 	
+	#Removes a user entry.
+	def removeUser (self, username):
+		print "-----"
+		print "Removing user %s" %(username)
+		#TODO: Test if the triggers work
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		c.execute ("delete from users where username=?", (username,))
+		conn.commit()
+		c.close()
+		
+	#adds a new policy
+	def addPolicy (self, policy):
+		print "-----"
+		print "Add a new policy: " %(policy)
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		c.execute("insert into policies values (null, ?)", (policy,))
+		conn.commit()
+		c.close()
+		return policyId
+	
+	#Removes a policy
+	def removePolicy (self, policy):
+		print "-----"
+		#TODO, check if triggers work
+		print "Remove an existing policy: " %(policy)
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		c.execute("delete from policies where policyData=?", (policy,))
+		conn.commit()
+		c.close()
+		return policyId
+		
+	#Links a public key with a given user
+	def addUserKey (self, signpostName, key):
+		print "-----"
+		print "Ready to add key to user %s" %(signpostName)
+		#Considering at the moment that there's a key per signpostName
+		#TODO, if there's one per user, then we need to update all the
+		#entries for the user that is linked to this signpost name
+		#It depends on what we will agree
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		now = datetime.datetime.now()
+		c.execute('update userIds set publickey=?, timestamp=? where signpostname=?', (key, now.strftime("%Y-%m-%d %H:%M:%s"), signpostName, ))
+		conn.commit()
+		c.close()
+	
+	#Cleans a key associated to a user
+	def cleanUserPublicKey (self, signpostName):
+		print "-----"
+		#NOTE: it doesn't remove the full entry, Just removes the public key
+		self.linkKeyToUser(signpostName, None)
+		print "Public key associated with %s was cleaned" %(signpostName)
+		
+	#Returns a public key for a given signpostName. 
+	#TODO: If there's any change on whether the keys are associated to users or to signpost names, 
+	#(i.e. per device), it should be done here
+	def getKeyFromUser (self, signpostName):
+		print "-----"
+		print "getKeyFromUser %s" %(signpostName)
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		c.execute("select publickey from userIds where signpostname=?", (signpostName,))
+		key = c.fetchone()
+		conn.commit()
+		c.close()
+		print "key value: %s" %(key)
+		return key
+	
+	#Returns the policyId for a given policy
+	def getPolicyId (self, policy):
+		print "-----"
+		print "Getting the policyID for policy: %s" %(policy)		
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		c.execute("select policyId from policies where policyData=?", (policy,))
+		policyId = c.fetchone()
+		c.close()
+		print "PolicyId for %s: %s" %(policy, policyId)
+		return policyId
+		
+	#Returns the policy value for a given policyId
+	def getPolicy (self, policyId):
+		print "-----"
+		print "Getting policy for policyId: %s" %(policyId)		
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		c.execute("select policyData from policies where policyId=?", (policyId,))
+		policy = c.fetchone()
+		print "Policy associated with PolicyId %s: %s" %(policyId, policy)
+		return policy
+	
+	#Returns a list of policyIds assigned to a given user
+	#Returns just the integers, in order to retrieve its value, use getPolicyValue
+	def getUserPolicyIds (self, username):
+		print "-----"
+		print 'Finding user policies associated with user: %s' %username
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		#Need to find userId first
+		userId = getUserIdFromSignpostName(username)
+		policies = None
+		if not userId is None:
+			c.execute("select policyId from userpolicies where userId=?", (userId))
+			policies = c.fetchall()
+		c.close()
+		print "----"
+		print "Policies assigned to user %s:" %(username)
+		for policy in policies:
+			print "PolicyId: %s" %(policy)			
+			#TODO: we need to make some decission process depending on how restrictive they are
+			#in order to return the most restrictive one
+		return policies
+			
+	#links a policy for a given user
+	def linkPolicyToUser(self, userName, policyId):
+		print "-----"
+		print "Maps policyId %s with user: %s" %(policyId, userName)
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		#Get userId		
+		userId = getUserIdFromSignpostName(username)
+		if not userId is None:
+			#Assume that there isn't any entry already
+			c.execute("insert into userpolicies values (?, ?)", (userId, policyId,))
+			conn.commit()
+		c.close()
+	
+	#Removes a given policy associated with a user
+	def removePolicyToUser(self, userName, policyId):
+		print "-----"
+		print "Removes Policy %s for %s" %(policyId, userName)
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		#Get userId		
+		userId = getUserIdFromSignpostName(username)
+		if not userId is None:
+			c.execute("delete from userpolicies where userId=?, policyId=?", (userId, policyId, ))
+			conn.commit()
+		c.close()
+	
+	#Removes all the policies for a given user
+	#TODO
+	def cleanAllUserPolicies (self, userName):
+		print "-----"
+		print "Clean all Policies for %s" %(userName)
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		#Get userId		
+		userId = getUserIdFromSignpostName(username)
+		if not userId is None:
+			c.execute("delete from userpolicies where userId=?", (userId,))
+			conn.commit()
+		c.close()
+		
+		
+	#Returns the userId for a userName
+	def getUserIdFromSignpostName (self, userName):
+		print "-----"
+		print 'Finding userId for %s' %signpostName
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		c.execute("select userId from users where name=?", (userName, ))
+		userId = c.fetchone()
+		c.close()
+		print "UserId for userName %s: %s" %(signpostName, userId)
+		return userId
+		
+	#Returns the userId for a signpostName
+	def getUserIdFromSignpostName (self, signpostName):
+		print "-----"
+		print 'Finding userId for %s' %signpostName
+		conn = sqlite3.connect(self.dbPath)
+		c = conn.cursor()
+		c.execute("select userId from userIds where signpostname=?", (signpostName, ))
+		userId = c.fetchone()
+		c.close()
+		print "UserId for signpostName %s: %s" %(signpostName, userId)
+		return userId				
+	
 	#Prints the list of users stored in the database. Mainly for debugging purposes
 	def printContacts (self):
+		print "-----"
+		print "Printing all the users stored"
 		conn = sqlite3.connect(self.dbPath)
 		c = conn.cursor()
 		c.execute('select * from users')
@@ -74,6 +268,8 @@ class SqliteController:
 	
 	#Prints the list of signpostNames stored in the database. Mainly for debugging purposes
 	def printSignpostNames (self):
+		print "-----"
+		print "Printing all the signpost names stored"
 		conn = sqlite3.connect(self.dbPath)
 		c = conn.cursor()
 		c.execute('select * from userIds')
