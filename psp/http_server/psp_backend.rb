070546a44f6b9d163546be698dc33c4ce60b7fbe
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2011-12-30 09:11:02 +0000

    Reduced tabs for readability

--- psp/http_server/psp_backend.rb
+++ psp/http_server/psp_backend.rb
@@ -1,55 +1,55 @@
 require 'http_server/psp_connection'
 
 module Thin
-								module Backends
-																# Backend to act as a TCP socket server.
-																class PspServer < Base
-																								# Address and port on which the server is listening for connections.
-																								attr_accessor :host, :port
+  module Backends
+    # Backend to act as a TCP socket server.
+    class PspServer < Base
+      # Address and port on which the server is listening for connections.
+      attr_accessor :host, :port
 
-																								def initialize(host, port, options)
-																																puts "this is the psp backend firing!!! yeah!!!!\n"
-																																@host = host
-																																@port = port
-																																super()
-																								end
+      def initialize(host, port, options)
+        puts "this is the psp backend firing!!! yeah!!!!\n"
+        @host = host
+        @port = port
+        super()
+      end
 
-																								# Connect the server
-																								def connect
-																																puts "psp connect method collaed\n"
-																																@signature = EventMachine.start_server(@host, @port, PspConnection, &method(:initialize_connection))
-																								end
+      # Connect the server
+      def connect
+        puts "psp connect method collaed\n"
+        @signature = EventMachine.start_server(@host, @port, PspConnection, &method(:initialize_connection))
+      end
 
-																								# Stops the server
-																								def disconnect
-																																EventMachine.stop_server(@signature)
-																								end
+      # Stops the server
+      def disconnect
+        EventMachine.stop_server(@signature)
+      end
 
-																								def to_s
-																																"#{@host}:#{@port}"
-																								end
+      def to_s
+        "#{@host}:#{@port}"
+      end
 
-																								protected
-																								# Initialize a new connection to a client.
-																								def initialize_connection(connection)
-																																connection.backend = self
-																																connection.app = @server.app
-																																connection.comm_inactivity_timeout = @timeout
-																																connection.threaded = @threaded
+      protected
+      # Initialize a new connection to a client.
+      def initialize_connection(connection)
+        connection.backend = self
+        connection.app = @server.app
+        connection.comm_inactivity_timeout = @timeout
+        connection.threaded = @threaded
 
-																																connection.start_tls(:private_key_file => 'ssl-keys/server.key.insecure', 
-																																																					:cert_chain_file => 'ssl-keys/server.crt', 
-																																																					:verify_peer => true)
+        connection.start_tls(:private_key_file => 'ssl-keys/server.key.insecure', 
+                                                  :cert_chain_file => 'ssl-keys/server.crt', 
+                                                  :verify_peer => true)
 
-																																# We control the number of persistent connections by keeping
-																																# a count of the total one allowed yet.
-																																if @persistent_connection_count < @maximum_persistent_connections
-																																								connection.can_persist!
-																																								@persistent_connection_count += 1
-																																end
+        # We control the number of persistent connections by keeping
+        # a count of the total one allowed yet.
+        if @persistent_connection_count < @maximum_persistent_connections
+          connection.can_persist!
+          @persistent_connection_count += 1
+        end
 
-																																@connections << connection
-																								end
-																end
-								end
+        @connections << connection
+      end
+    end
+  end
 end
--- psp/http_server/psp_connection.rb
+++ psp/http_server/psp_connection.rb
@@ -4,28 +4,28 @@ require 'thin/connection'
 #  https://localhost:8080/ -v 
 
 module Thin
-				# Connection between the server and client.
-				# This class is instanciated by EventMachine on each new connection
-				# that is opened.
-				class PspConnection < Thin::Connection
+  # Connection between the server and client.
+  # This class is instanciated by EventMachine on each new connection
+  # that is opened.
+  class PspConnection < Thin::Connection
 
-								# verify the ssl credentials. 
-								def ssl_verify_peer(cert)
-										certificate = OpenSSL::X509::Certificate.new cert
-										user = certificate.subject.to_a.select{|v| (v[0] == "CN")}
-										puts "request certificate for " + user[0][1]
-										puts "Success always when credentials are provided\n"
-										return true
-								end
+    # verify the ssl credentials. 
+    def ssl_verify_peer(cert)
+      certificate = OpenSSL::X509::Certificate.new cert
+      user = certificate.subject.to_a.select{|v| (v[0] == "CN")}
+      puts "request certificate for " + user[0][1]
+      puts "Success always when credentials are provided\n"
+      return true
+    end
 
 
-								# Make sure that when the user provides no credential tear down
-								# connection
-								def ssl_handshake_completed
-												if (get_peer_cert == nil) 
-																puts "no certificate found, terminating\n"	
-																close_connection()
-												end
-								end
-				end
+    # Make sure that when the user provides no credential tear down
+    # connection
+    def ssl_handshake_completed
+      if (get_peer_cert == nil) 
+        puts "no certificate found, terminating\n"	
+        close_connection()
+      end
+    end
+  end
 end
