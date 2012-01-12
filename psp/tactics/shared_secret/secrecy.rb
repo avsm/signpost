fe8f39aba2bb9f74413d5f8a38faaac5cfce6970
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2012-01-12 16:33:34 +0000

    [NOT FOR PROD!] Tactic for shared secrets
    This tactic is not ready for production use.
    It is very very much not secure in fact!
    The secrets can easily be generated by other users.
    This is just a dummy to get a secret to test with.

---
+++ psp/tactics/shared_secret/secrecy.rb
@@ -0,0 +1,30 @@
+#! /usr/bin/env ruby
+
+require 'rubygems'
+require 'digest/sha1'
+require 'base64'
+require 'bundler/setup'
+require 'net/dns/resolver'
+require 'lib/tactic_solver/tactic_helper'
+
+tactic = TacticHelper.new
+
+tactic.when :signpost_for_client do |helper, truths|
+  signpost_domain = truths[:signpost_for_client][:value]
+  truths[:resource][:value] =~ /shared_secret-([[:graph:]]*)/
+  service = $1
+  helper.log "Generating shared secret for #{service}"
+
+  # FIXME: This should be based on a shared key of some sort!
+  garb_secret = Digest::SHA1.hexdigest("key for #{signpost_domain} and service #{service}")
+  secret = Base64.encode64(garb_secret).chomp
+
+  a_year = 365 * 24 * 60 * 60
+  helper.provide_truth truths[:what][:value], secret, a_year, true
+
+  # Ready to generate more shared secrets :)
+  helper.recycle_tactic
+end
+
+# We need to initialize the tactic, otherwise nothing will ever happen
+tactic.run
