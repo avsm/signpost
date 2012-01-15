f0816b3c701eddfe3ca24d7e02d95bbae35a939f
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2012-01-15 18:28:32 +0000

    Better help description

--- tactics/iodine/start_client.sh
+++ tactics/iodine/start_client.sh
@@ -27,4 +27,20 @@ while getopts "n:" opt; do
   esac
 done
 
+echo
+echo "****************"
+echo "Once the tunnel is setup, the following IP's should be setup:"
+echo "  Server: 10.0.0.1"
+echo "  Client: 10.0.0.2"
+echo
+echo "If there are other clients, then restart the system so you get consistent performance"
+echo
+echo "!!!!!!!!"
+echo "It will ask you for your password so it can start iodine with root privileges"
+echo "!!!!!!!!"
+echo "****************"
+echo
+echo
+echo
+
 sudo iodine -f -P iodine_test $DOMAIN
