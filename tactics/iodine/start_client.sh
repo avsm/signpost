944f81b3ebe987fe679c8d96a1a9da213acd1f87
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2012-01-17 11:01:38 +0000

    Remove features not needed

--- tactics/iodine/start_client.sh
+++ tactics/iodine/start_client.sh
@@ -1,33 +1,6 @@
 #!/usr/bin/env bash
 # Start iodined server
-
-set -e
-
-function usage {
-  echo "-d the domain that iodined listens to"
-  echo 
-  echo "-h : display this message"
-  echo
-  echo "Will setup an iodined server"
-  exit 1
-}
-
-VERSION=`iodine -v`
-
 DOMAIN=i.d2.signpo.st
-while getopts "n:" opt; do
-  case $opt in
-    d)
-      DOMAIN="$OPTARG"
-      ;;
-    h)
-      usage
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
-  esac
-done
 
 echo
 echo "****************"
