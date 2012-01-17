d93be25146ed822b26058aff69e5b3090ac583cd
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2012-01-17 15:07:04 +0000

    Adding interfaces to ptunnels to enable packet cap

--- tactics/ptunnel/start_client.sh
+++ tactics/ptunnel/start_client.sh
@@ -2,7 +2,11 @@
 
 # Start ptunnel client
 
+echo "Usage: $0 <proxy_url> <interface>"
+echo
+
 PROXY=$1
+INTERFACE=$2
 PORT=5000
 HOST="127.0.0.1"
 PASSWORD="ptunnel_eval_password"
@@ -11,7 +15,8 @@ echo
 echo "****************"
 echo "Connecting to $PROXY"
 echo "Local port $PORT is relaying to $HOST:$PORT"
+echo "Packet capture on device $INTERFACE"
 echo "Using password: $PASSWORD"
 echo "****************"
 echo
-sudo ptunnel -p $PROXY -lp $PORT -da $HOST -dp $PORT -x $PASSWORD
+sudo ptunnel -c $INTERFACE -p $PROXY -lp $PORT -da $HOST -dp $PORT -x $PASSWORD
