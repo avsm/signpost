09d941b973c844180e56115f78af6616dd54cff4
Narseo Vallina Rodríguez <Narseo@narseo.(none)>
2012-01-18 21:22:00 +0000

    changes in eval script

--- tactics/evals/simpleSignpostBandwidthEval_client.sh
+++ tactics/evals/simpleSignpostBandwidthEval_client.sh
@@ -10,8 +10,29 @@ function usage {
   exit 1
 }
 
+#Define outputFileName
+#Get tunnel description
+tunnel=$3
+ping_output="ping_client_"$tunnel".ping"
+iperf_output="iperf_client_"$tunnel".iprf"
+tcpdump_output="tdmp_client_"$tunnel".pcap"
 
-MAX_ATTEMPTS="10"
+#Run tcpdump on interface
+tcpdump -w $tcpdump_output - i $4
+
+echo "TUNNEL: $tunnel" >> $ping_output
+echo "$(date +%Y%m%d%H%M%S) >> $ping_output
+echo "Remote: $1" >> $ping_output
+echo "TUNNEL: $tunnel" >> $iperf_output
+echo "$(date +%Y%m%d%H%M%S) >> $iperf_output
+echo "Remote: $1" >> $ipef_output
+
+
+echo "Output ping file: $ping_output"
+echo "Output iperf file: $iperf_output"
+
+#Execution parameters
+MAX_ATTEMPTS="5"
 LISTENING_PORT="6650"
 IPERF_INTERVAL="1"
 IPERF_DUTY_CYCLE="5"
@@ -29,7 +50,10 @@ do
   #This can be considered as for how long we want a test on a given link
   echo "New connection: $currentIter"
   currentIter=$(( $currentIter + 1 ))
-  iperf -c $SERVER_DOMAIN -p $LISTENING_PORT -t $IPERF_DUTY_CYCLE -i $IPERF_INTERVAL >> outIperf.txt &
-  ping -i $IPERF_INTERVAL -c $PING_MAX $SERVER_DOMAIN >> outPing.txt &
+  iperf -c $SERVER_DOMAIN -p $LISTENING_PORT -t $IPERF_DUTY_CYCLE -i $IPERF_INTERVAL | tee $iperf_output &
+  ping -i $IPERF_INTERVAL -c $PING_MAX $SERVER_DOMAIN | tee $ping_output &
   wait
 done
+killall tcpdump
+killall iperf
+killall ping
