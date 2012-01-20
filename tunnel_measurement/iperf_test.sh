89c50ccf722366e88f2ebf2f40fed4f98a2e1071
root <root@quorum103.cl.cam.ac.uk>
2012-01-20 10:05:35 +0000

    Added dual port test for iperf udp and an extra param for the pttcp to define the base port

--- tunnel_measurement/iperf_test.sh
+++ tunnel_measurement/iperf_test.sh
@@ -9,9 +9,10 @@ mon_intf=$3
 loc=aws
 loc_ip=$4
 loc_port=$5
-duration=60
-dst_loc=$6
-pttcp_dir=$7
+base_port=$6
+duration=20
+dst_loc=$7
+pttcp_dir=$8
 
 # chec ig params are correct
 if [ ! -e $dst_loc ]; then 
@@ -24,6 +25,7 @@ echo starting iperf session
 /usr/sbin/tcpdump -i $mon_intf -w $loc-iperf_tcp-$tunnel_name-$timestamp.pcap &
 ping $loc_ip | tee $loc-ping_iperf_tcp-$tunnel_name-$timestamp.txt &
 
+echo iperf -c $loc_ip -i 1 -t $duration -p $loc_port -d > test_output.txt 
 iperf -c $loc_ip -i 1 -t $duration -p $loc_port -d | tee $loc-iperf_tcp-$tunnel_name-$timestamp.txt &
 
 sleep $duration;
@@ -44,7 +46,7 @@ cp $loc-ping_iperf_tcp-$tunnel_name-$timestamp.txt $dst_loc/
 ping $loc_ip | tee $loc-ping_pttcp-$tunnel_name-$timestamp.txt &
 
 $pttcp_dir/pttcp -c $loc_ip --interpage pareto 25 2 \
-  --objsize pareto 12288 1.2 -B 6000 -N 30	| tee $loc-pttcp-$tunnel_name-$timestamp.txt &
+  --objsize pareto 12288 1.2 -B $base_port -N 11 | tee $loc-pttcp-$tunnel_name-$timestamp.txt &
 
 sleep $duration;
 
@@ -65,7 +67,7 @@ echo starting streaming test;
 ping $loc_ip | tee $loc-ping_iperf_udp-$tunnel_name-$timestamp.txt &
 
 # dvd quality
-iperf -c $loc_ip -p $loc_port -b 5700K -i 1 -t $duration | tee $loc-iperf_udp-$tunnel_name-$timestamp.txt &
+iperf -c $loc_ip -p $loc_port -b 5700K -i 1 -t $duration -d | tee $loc-iperf_udp-$tunnel_name-$timestamp.txt &
 
 sleep $duration;
 
