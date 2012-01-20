4b2e921beab53cdd6a77c9c9f905c93f50457a2e
root <root@quorum103.cl.cam.ac.uk>
2012-01-20 10:04:16 +0000

    Need also to run ssh to port forward ports

--- tunnel_measurement/iperf_server.sh
+++ tunnel_measurement/iperf_server.sh
@@ -5,3 +5,4 @@ loc_port=80
 screen -dmS iperf_tcp sudo iperf -s -p $loc_port
 screen -dmS iperf_udp sudo iperf -s -u -p $loc_port
 screen -dmS pttcp echo need to see how I run this
+ssh -i ~/.ssh/id_rsa -L 5001:localhost:5001 -L  6000:localhost:6000   -L  6001:localhost:6001  -L  6002:localhost:6002  -L  6003:localhost:6003  -L  6004:localhost:6004 cr409@chicane
