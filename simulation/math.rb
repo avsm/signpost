fb448217e1639ba7039ac4b2e886fa9decbcb3cf
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2012-01-25 14:58:46 +0000

    Changes to assumptions.
    Stuff to remember for the future, in case it is needed.

--- simulation/math.rb
+++ simulation/math.rb
@@ -16,7 +16,7 @@
 # For this reason I am not modeling the increase of requests as seen by the
 # root or name servers.
 # 
-# All signpost requests will on the other hand pass through local resolvers
+# On the other hand, all signpost requests will pass through local resolvers
 # that either recursively resolve the requests, or proxy them to another
 # resolver. It is therefore interesting to see the increase in DNS requests
 # that occurr as results of different quantities and configurations of signpost
@@ -25,54 +25,105 @@
 # --------------------------------------------------------------------------
 # Simplifying assumptions:
 #
-# - Assumption: The number of devices per user is the same system wide.
+# - Assumption 1: The number of devices per user is the same system wide.
 #   Justification: As long as the number of devices is an upper bound, this
-#   provides a safe over estimation.
+#     provides a safe over estimation.
 #
-# - Assumption: The number of devices behind each resolver (or proxy) is the same.
+# - Assumption 2: The number of devices behind each resolver (or proxy) is the same.
 #   Justification: The increase in traffic is proportional to the number of
-#   devices behind a resolver, but since resolvers also have processing capacity
-#   proportional to the number of devices they are supposed to front, they will
-#   see the same proportional increase in traffic, which should not cause
-#   problems, assuming the remaining infrastructure can handle it.
+#     devices behind a resolver, but since resolvers also have processing capacity
+#     proportional to the number of devices they are supposed to front, they will
+#     see the same proportional increase in traffic, which should not cause
+#     problems, assuming the remaining infrastructure can handle it.
 #
-# - Assumption: All a users signposts are spread behind different resolvers.
+# - Assumption 3: All a users signposts are spread behind different resolvers.
 #   Justfication: We are considering the case where all signposts synchronize
-#   directly with one of the publicly accessible signposts, and not at the edge
-#   amongst each other. This is a worst case scenario in terms of the
-#   amount of traffic, providing an upper bound. Since all communication
-#   happens with a public signpost, the placement of individual signposts
-#   becomes irrelevant.
+#     directly with one of the publicly accessible signposts, and not at the edge
+#     amongst each other. This is a worst case scenario in terms of the
+#     amount of traffic, providing an upper bound. Since all communication
+#     happens with a public signpost, the placement of individual signposts
+#     becomes irrelevant.
 #
-# - Assumption: All users have friends they communicate with, and communicate
-#   with each one in different amounts. Since all the signposts are spread
-#   out evenly behind different resolvers, and all signposts speak to different friends
-#   at the same time, the assumption is made that this averages out amongst the
-#   resolvers.
-#   Justification: ?
+# - Assumption 4: All users have friends they communicate with, and communicate
+#     with each one in different amounts. We use the simplifying assumption
+#     that all signposts on average receive the same amount of requests from
+#     foreign signposts.
+#   Justification: Since all the signposts are spread out evenly behind different 
+#     resolvers, and all signposts speak to different friends at the same time, 
+#     the amount of traffic a signpost receives from foreign domains averages
+#     out. 
 #
-# - Assumption: A signpost request costs four times as much as a regular DNS
-#   request.
+# - Assumption 5: A signpost request costs four times as much as a regular DNS
+#     request.
 #   Justification: TCPDumps from requests made of typical signpost DNS requests
-#   sent over iodine, show that a single request regularly produce 3 iodine DNS
-#   requests. Using 4 is a safe overestimation.
+#     sent over iodine, show that a single request regularly produce 3 iodine DNS
+#     requests. Using 4 is a safe overestimation.
+#     A typical signpost request in this case is a request using the signpost
+#     DNS extensions of including amongst other a TXT record with the addresses
+#     of the requester as well as KEY material and TSIG records.
+#   Implication:
+#     sp_dns_cost_multiple = 4
 #
-# - Assumption: Iodine tunnels have a fixed overhead of 1 packet per signpost
-#   device per 4 seconds.
+# - Assumption 6: Iodine tunnels have a fixed overhead of 1 packet per signpost
+#     device per 4 seconds.
 #   Justification: Empirical measurments verify this.
+#   Implication:
+#     packets_per_second_for_iodine_maintenance = 1/4
 #
-# - Assumption: One signpost request causes requests of equal size to be
-#   generated to each of the other signpost devices in the domain to sync
-#   state.
+# - Assumption 7: One signpost request causes requests of equal size to be
+#     generated to each of the other signpost devices in the domain to sync
+#     state.
 #   Justification: This is a safe overestimation, as given the same data, all
-#   signposts can calculate the same changes and maintain the same state.
+#     signposts can calculate the same changes and maintain the same state.
+#   Implication:
+#     ratio_sync_to_request = 1
 #
-# - Assumption: One signpost contacting a singpost in another domain causes
-#   a state broadcast in both signpost domains.
+# - Assumption 8: There is an even load generated across the board, that is
+#     irrespective of which signpost domain or device the traffic is generated
+#     from.
+#   Justification: We are assuming all signposts create the same amount of DNS
+#     traffic. This might not be true, but by setting the amount of traffic
+#     generated sufficiently high, this is a safe overestimation.
+#
+# - Assumption 9: Instead of looking at individual signpost domains requests and
+#     replies, we can generalise across all signpost domains, and just use the
+#     fraction of signpost enabled devices that are at the edge.
+#   Justification: By the assumption above that all signpost devices generate
+#     an even load, we have that the magnitude of the load is irrespective of
+#     which signpost domain the requester is in. This is because all devices,
+#     irrespective of domain generate this average amount of traffic, which
+#     also implies that all devices, irrespective of domain, will receive the
+#     same amount of incoming sync traffic.
+#
+# - Assumption 10: One signpost contacting a singpost in another domain causes
+#     a state broadcast in both signpost domains. More specifically the
+#     increase in synchronisation traffic is by a factor of two.
 #   Justification: setting up a tunnel to a foreign signpost causes tunnels
-#   ends to be set up in both signpost domains. Hence a broadcast or sync of
-#   state will occurr. Setting this to be the same as the size of the message
-#   is again a safe overestimation.
+#     to be set up in both signpost domains. Hence a broadcast or sync of
+#     state will also occurr in both domains. Since the material synchronized
+#     in both domains is of the same nature, the order of magnitude of the 
+#     traffic overhead is the same in both domains. Hence the factor of two.
+#
+# - Assumption 11: We can safily ignore the factor of two in increase of sync
+#     overhead generated by requests to external signpost domains.
+#   Justification: From Assumption 8 we know that each signpost device
+#     generates the same amount of outwards traffic. From Assumption 4 we know
+#     that the amount of external traffic a signpost receives also is
+#     irrespective of the individual signpost. This allows us to swallop up the
+#     increased cost of synchronisation for foreign requests in the constant
+#     that relates the cost of a signpost request to the cost involved in
+#     syncing said request.
+#   Implication: existance of variable ratio_sync_to_request
+#
+# - Assumption 12: It doesn't matter how many signposts an individual user has.
+#     The only thing that matters is what percentage of devices are signposts.
+#   Justification: Since we are making the simplifying assumptions that all
+#     a users devices are between different resolvers (Assumption 3), and also
+#     that the traffic generated in a sense is irrespective of the users device
+#     (Assumption 8), the number of signposts each individual user has does not
+#     matter. On the other hand, the percentage of signposts system wide
+#     matters, as it determines the fraction of extra traffic that is
+#     generated.
 #
 
 
@@ -82,6 +133,9 @@ def gen_load(options)
 
   # Devices per user
   dpu = options[:dpu] || 2
+
+  # Fraction of all clients that are signposts
+  fsp = options[:fsp] || 0.2
 
   # Fraction of a users signpost that is in the cloud
   fspc = options[:fspc] || 0.8
@@ -100,21 +154,36 @@ def gen_load(options)
   # rounding errors it does slightly change the final outcome.
   dns_load = 1_000_000
 
-  # Fraction of signpost communication that is internal, i.e. trying to establish
-  # routes to other internal signposts, vs communication that is against
-  # signposts in other domains, i.e. trying to establish a tunnel with a foreign
-  # signpost.
-  fraction_internal = options[:fraction_internal] || 0.5
+  # Amount of data that has to be sync'ed per signpost device compared to the
+  # amount of data sent out in a signpost request.
+  ratio_sync_to_request = options[:ratio_sync_to_request] || 1
+
+  # Packets sent per second to maintain an iodine tunnel
+  packets_per_second_for_iodine_maintenance = 1.0/4
 
   #-------------------------------------------------------------
 
+  # The number of signposts.
+  num_sp = dpr * fsp
+
   # Signposts at edge
   # Number of devices at the router, minus the ones that are in the cloud.
-  sp_edge = dpu - (dpu * fspc)
+  # TODO: FOR FUTURE
+  #   We should be considering percentage of machines that are signposts.
+  #   That results in the following equations:
+  #
+  #     num_sp = (edge_not_sp + sp_edge + sp_cloud) * fsp
+  #     edge_not_sp = dpr - sp_edge
+  #     sp_cloud = num_sp * frCloud
+  #     sp_edge = num_sp - sp_cloud
+  #
+  #   Solving is an exercise for the reader.
+  #
+  sp_edge = dpr - (dpu * fspc)
 
   # Overhead caused by iodine is one packet every 4 seconds per signpost device
   # at the edge
-  iodine_overhead = 1.0/4 * sp_edge
+  iodine_overhead = packets_per_second_for_iodine_maintenance * sp_edge
 
   # The normal load of DNS traffic
   # Each device at the resolver sends out dns_load packages per second.
@@ -132,30 +201,22 @@ def gen_load(options)
   # about setting up tunnels to external signpost domains.
   signpost_requests_cost = sp_edge * sp_request_cost
 
-  # The cost of the request sent to internal signposts.
-  internal_sp_requests_cost = signpost_requests_cost * fraction_internal
-
-  # Overhead from the internal communication from requests by signposts within
-  # the same domain. Since each signpost request going out is broadcast back to
-  # all the other signposts in the same domain, we have an additional message
-  # being sent to all the signposts at the edge on average per signpost sent
-  # message.
-  internal_sp_req_sync_overhead = internal_sp_requests_cost * sp_edge
-
-  # The cost of sending messages to foreign signposts is the total cost of
-  # signpost requests, minus the cost of the messages that are for internal
-  # communication.
-  foreign_sp_req_sync_cost = signpost_requests_cost - internal_sp_requests_cost
-
-  # Overhead of communicating with external signposts. Communication with
-  # external signposts is extra costly, because it causes sync traffic both
-  # within my own signpost domain, but also within the signpost domain of the
-  # signpost being contacted. Hence sp_edge^2
-  foreign_sp_req_sync_overhead = foreign_sp_req_sync_cost * sp_edge * sp_edge
-
-  # Overhead caused by keeping signposts in sync
-  sync_overhead = internal_sp_req_sync_overhead +  # sync resulting from internal communication
-                  foreign_sp_req_sync_overhead     # sync resulting from foreign signposts contacting us
+  # The overhead generated by signpost communication. It has two parts:
+  # Internal communication: A device has contacted another device in the same
+  #     signpost domain to propagate some state, for example that it has moved
+  #     and is now addressable using a different address. This information then
+  #     has to be propagated to all other signposts in the same domain.
+  # External communication: A device contacts a different signpost domain to
+  #     have it setup a tunnel between the domains. This causes traffic to be
+  #     generated in both signpost domains. In both cases all signposts in both
+  #     domains are informed about new tunnels having/being set up, and other
+  #     related information.
+  # The external communication is greater by a factor of two, since it is
+  # involving the devices in both signpost domains.
+  # But since we have no clear idea of the relationship between internal and
+  # external traffic, we leave it out.
+  # IS THIS SOUND?
+  sync_overhead = signpost_requests_cost * sp_edge * ratio_sync_to_request
 
   # The load when signpost systems are in use
   resolver_signpost_load = resolver_base_load +
@@ -169,11 +230,11 @@ def gen_load(options)
 end
 
 options = {
-  :dpr => 100,
-  :dpu => 2,
-  :fspc => 0.8,
-  :amount_of_signpost_traffic => 0.1,
-  :fraction_internal => 0.1
+  :dpr => 100, # Devices per resolver
+  :dpu => 2, # Devices per user
+  :fspc => 0.8, # Fraction of a users signposts that are in the cloud
+  :amount_of_signpost_traffic => 3, # How many times the normal DNS load is generated by singposts
+  :ratio_sync_to_request => 1 # How much more heavy a sync message is compared to a signpost request
 }
 
 # We want data for three scenarios,
